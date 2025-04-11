48a46577450608624ad741376b36c049102e2a79
Quyentrinhhong <hongquyen.20603@gmail.com>
2025-04-11 16:26:02 +0700

    edit

--- TrinhHongQuyen_2122110059/Controllers/DashboardController.cs
+++ TrinhHongQuyen_2122110059/Controllers/DashboardController.cs
@@ -30,6 +30,13 @@ namespace TrinhHongQuyen_2122110059.Controllers
             return View(viewModel);
         }
 
+        // Danh sách
+        public IActionResult BannerList()
+        {
+            var banners = _context.Banners.ToList();
+            return View(banners);
+        }
+
         // Thêm Banner
         public IActionResult AddBanner()
         {
@@ -43,7 +50,7 @@ namespace TrinhHongQuyen_2122110059.Controllers
             {
                 _context.Banners.Add(banner);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("BannerList");
             }
             return View(banner);
         }
@@ -52,8 +59,6 @@ namespace TrinhHongQuyen_2122110059.Controllers
         public IActionResult EditBanner(int id)
         {
             var banner = _context.Banners.Find(id);
-            if (banner == null)
-                return NotFound();
             return View(banner);
         }
 
@@ -64,7 +69,7 @@ namespace TrinhHongQuyen_2122110059.Controllers
             {
                 _context.Banners.Update(banner);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("BannerList");
             }
             return View(banner);
         }
@@ -73,17 +78,128 @@ namespace TrinhHongQuyen_2122110059.Controllers
         public IActionResult DeleteBanner(int id)
         {
             var banner = _context.Banners.Find(id);
-            if (banner != null)
-            {
-                _context.Banners.Remove(banner);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Index");
+            _context.Banners.Remove(banner);
+            _context.SaveChanges();
+            return RedirectToAction("BannerList");
+        }
+        // Category 
+
+        // Danh sách
+        public IActionResult CategoryList()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
         }
 
-        // Các phương thức tương tự cho User, Category và Product
+        // Thêm Category
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
 
-        // Thêm User
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("CategoryList");
+            }
+            return View(category);
+        }
+
+        // Sửa category
+        public IActionResult EditCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult EditCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("CategoryList");
+            }
+            return View(category);
+        }
+
+        // Xóa category
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        // PRODUCT
+        //
+        // Danh sách
+        public IActionResult ProductList()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        // Thêm Product
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("ProductList");
+            }
+            return View(product);
+        }
+
+        // Sửa product
+        public IActionResult EditProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("ProductList");
+            }
+            return View(product);
+        }
+
+        // Xóa product
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        // user
+        // Danh sách
+        public IActionResult UserList()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+
+        // Thêm AddUser
         public IActionResult AddUser()
         {
             return View();
@@ -96,17 +212,15 @@ namespace TrinhHongQuyen_2122110059.Controllers
             {
                 _context.Users.Add(user);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("UserList");
             }
             return View(user);
         }
 
-        // Sửa User
+        // Sửa user
         public IActionResult EditUser(int id)
         {
             var user = _context.Users.Find(id);
-            if (user == null)
-                return NotFound();
             return View(user);
         }
 
@@ -117,25 +231,20 @@ namespace TrinhHongQuyen_2122110059.Controllers
             {
                 _context.Users.Update(user);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("UserList");
             }
             return View(user);
         }
 
-        // Xóa User
+        // Xóa user
         public IActionResult DeleteUser(int id)
         {
             var user = _context.Users.Find(id);
-            if (user != null)
-            {
-                _context.Users.Remove(user);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Index");
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            return RedirectToAction("UserList");
         }
-
-        // Thêm Category, Sửa Category, Xóa Category tương tự như trên
-
-        // Thêm Product, Sửa Product, Xóa Product tương tự như trên
     }
+
 }
+
